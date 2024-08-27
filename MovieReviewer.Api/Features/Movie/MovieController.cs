fd12d98e37289d82892715dbb383dede47d93b9e
Abubakar Ahmed <abubakarmahfooz4479@outlook.com>
2024-08-27 11:01:30 +0530

    Added a wasm & server mud blazor client and trying to add movie to display

--- MovieReviewer.Api/Features/Movie/MovieController.cs
+++ MovieReviewer.Api/Features/Movie/MovieController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using Ardalis.Result;
 using Ardalis.Result.AspNetCore;
